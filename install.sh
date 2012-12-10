a71da6d427d0a4304688831bab2ad3e27d992a27
Joe Papperello <joe@socialflow.com>
2012-12-10 23:22:48 +0000

    updating install script

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 DIR="$(pwd)"
 	ln -sf "$DIR/vimrc" "$HOME/.vimrc"
 	ln -sf "$DIR/tmux.conf" "$HOME/.tmux.conf"
-	ln -sf "$DIR/gitconfig.conf" "$HOME/.gitconfig"
+	ln -sf "$DIR/gitconfig" "$HOME/.gitconfig"
     mkdir -p "$HOME/.vim/colors"
     ln -sf "$DIR/vim-colors/molokai_mod.vim" "$HOME/.vim/colors/molokai_mod.vim"
