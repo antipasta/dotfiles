d716adb05ed35a86e94c29b4cecb1c79b6728877
Joe Papperello <joe@socialflow.com>
2015-01-31 11:00:12 -0500

    sqliterc

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 DIR="$(pwd)"
 	ln -sf "$DIR/vimrc" "$HOME/.vimrc"
+	ln -sf "$DIR/sqliterc" "$HOME/.sqliterc"
 	ln -sf "$DIR/tmux.conf" "$HOME/.tmux.conf"
 	ln -sf "$DIR/gitconfig" "$HOME/.gitconfig"
     mkdir -p "$HOME/.vim/colors"
