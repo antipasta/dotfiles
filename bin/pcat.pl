f1ce7f8cc73124fddec1df8e7b56dc0d10523c8a
Joe Papperello <joeyis@gmail.com>
2024-04-22 09:15:59 -0400

    remove junk i no longer need

--- bin/pcat.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env perl
-use strict;
-use warnings;
-
-my $a = $ARGV[0];
-my ($host,$port) = split(/:/,$a);
-warn $host;
-my $vpn = ($host =~ /mars.sfsrv.net/) ? 'mars' : 'saturn';
-`socat TCP-LISTEN:$port,reuseaddr,fork 'EXEC:docker exec -i $vpn "nc $host $port"'`;
